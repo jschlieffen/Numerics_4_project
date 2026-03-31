1ac673514bbd97e77d491dfc091eae3ad188f912
bolkareren <bolkar56@gmail.com>
2026-03-31 15:53:30 +0200

    extending to model functions and code optimizations to simulation

--- co_evolution_modified.py
+++ co_evolution_modified.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime
 from typing import Callable
+from scipy.special import expit
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -17,6 +20,22 @@ Created on Fri Jan 30 16:13:54 2026
 @author: jschlieffen
 """
 
+global HIST_BINS;
+HIST_BINS = np.array([-3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5])
+
+def opinion_sampler(initial_opinion_data: np.ndarray, num_agents: int) -> np.ndarray:
+    # Japan data is in 7 bins, we can force the opinions
+    # Bound the opinions to be between -3.5 and 3.5, sample using initial opinion percentages
+    edges = HIST_BINS
+    probs = np.array(initial_opinion_data, dtype=float)
+    probs = probs/probs.sum()
+    
+    sampled_opinions = np.random.choice(len(probs), size=num_agents, p=probs)
+    left_edges = edges[sampled_opinions]
+    right_edges = edges[sampled_opinions + 1]
+    # Sample uniformly between the edges to get continuous opinions
+    return np.random.uniform(left_edges, right_edges)
+
 class opinion_dynamics:
     
     inf_rate_max_plus_min: float
@@ -28,8 +47,8 @@ class opinion_dynamics:
 
 
     def __init__(self, num_grid_points: int, max_t: int, initial_opinions: np.ndarray, y0: np.ndarray, N:int,
-                 interaction_distance: float, noise_strength: float, stochiomatric_vectors: np.ndarray, grad_V: Callable,
-                 inf_rate_max: float = 0.35, inf_rate_min: float = 0.0225, rec_rate: float = 0.01):
+                 interaction_distance: float, noise_strength: float, stochiomatric_vectors: np.ndarray, grad_V: str,
+                 grad_V_params: tuple = (0, 0), inf_rate_max: float = 0.35, inf_rate_min: float = 0.0225, rec_rate: float = 0.01):
         self.t = np.linspace(0, max_t, num_grid_points)
         self.dt = max_t/num_grid_points
         self.inf_rate_max_plus_min = inf_rate_max + inf_rate_min
@@ -50,6 +69,7 @@ class opinion_dynamics:
         self.N = N
         self.d = interaction_distance
         self.grad_V = grad_V
+        self.grad_V_params = grad_V_params
 
     def algo(self):
         for i in range(1, len(self.t)):
@@ -57,8 +77,17 @@ class opinion_dynamics:
             self.infection_num_array[i] = self.update_infected()
             self.infection_array[i] = np.copy(self.infection_curr)
             # Apply grad_V to each agent with current infected numbers
-            drift_V = self.grad_V(self.opinions_curr, self.infection_curr[1]/self.N)
-            self.opinions_curr += -drift_V * self.dt
+            # drift_V = self.grad_V(self.opinions_curr, self.infection_curr[1]/self.N)
+            # self.opinions_curr += -drift_V * self.dt
+            
+            infected_frac = self.infection_curr[1] / self.N
+            if self.grad_V == "sigmoid":
+                self.opinions_curr -= self.grad_V_params[0] * (1.0 - infected_frac) - self.grad_V_params[1] * infected_frac * expit(self.opinions_curr) * self.dt
+            elif self.grad_V == "polynomial":
+                alpha = (1 - 2 * self.grad_V_params[0])/(self.grad_V_params[0]**2 - self.grad_V_params[0])
+                inf_func = alpha * infected_frac**2 + (2 - alpha) * infected_frac - 1
+                self.opinions_curr -= 4 * self.opinions_curr * (self.opinions_curr**2 + 1) - 8 * self.grad_V_params[1] * inf_func(infected_frac) * self.opinions_curr**2 * self.dt
+            
             self.opinions_curr = np.clip(self.opinions_curr, -3.5, 3.5)
             self.opinion_array[i] = np.copy(self.opinions_curr)
 
@@ -94,38 +123,62 @@ class opinion_dynamics:
         return self.infection_num_array
 
 def main():
+    # Data loading
+    start_date = datetime(2020, 3, 12)
+    end_date = datetime(2020, 6, 26)
+    infection_data = pd.read_csv("data/japan-covid-data.csv", index_col="date", parse_dates=["date"])
+    opinion_data = pd.read_csv("data/zib/Opinion_data/survey_data_original_JPN.csv", index_col="Date", parse_dates=["Date"])
+    infection_data = infection_data.loc[start_date:end_date]
+    opinion_data = opinion_data.loc[start_date:end_date]
+    
+    # Total cases at the start date is used as initial number of infected for the simulation
+    initial_infected = infection_data['total_cases'].iloc[0]
+    # Reindex opinions to match the overall timescale
+    opinion_data = opinion_data.reindex(infection_data.index)
+    opinion_data = opinion_data.to_numpy()
+    # Save indices where opinion data was recorded originally
+    opinion_idx = np.where(~np.isnan(opinion_data).all(axis=1))[0]
+    # Save indices where infection data is recorded (0 means not recorded)
+    infection_idx = (infection_data["new_cases"] > 0).to_numpy().nonzero()[0]
+    infection_data = (infection_data['new_cases']).to_numpy()
+    
     # Parameters
-    N = 100000
-    initial_inf = 1075
-    sim_length = 107
-    initial_opinions = np.random.uniform(-1, 1, size=N)
-    grad_V = np.vectorize(lambda opinion, infected: 0)
+    N = 300000
+    sim_to_data_ratio = 3
+    sim_length = len(opinion_data) * sim_to_data_ratio
+    initial_opinion = opinion_sampler(opinion_data[0], N)
+    params = (0, 0)
+    
     model = opinion_dynamics(
         num_grid_points=sim_length,
         max_t=sim_length,
-        initial_opinions=initial_opinions,
+        initial_opinions=initial_opinion,
         N=N,
-        y0=np.array([N - initial_inf, initial_inf, 0]),
+        y0=np.array([N - initial_infected, initial_infected, 0]),
         interaction_distance=0,
         noise_strength=0,
         stochiomatric_vectors=np.array([[-1, 1, 0], [0, -1, 1]]),
-        grad_V=grad_V,
-        inf_rate_max=0.666
+        grad_V="sigmoid",
+        grad_V_params=params,
+        inf_rate_max=0.4
     )
     model.algo()
-    plt.hist(model.opinion_history()[0], bins=5)
+    
+    plt.hist(model.opinion_history()[0], bins=tuple(HIST_BINS))
     plt.savefig('plots/other/initial_opinions.png')
     plt.close()
-    plt.hist(model.opinion_history()[-1], bins=5)
+    plt.hist(model.opinion_history()[-1], bins=tuple(HIST_BINS))
     plt.savefig('plots/other/final_opinions.png')
     plt.close()
-    plt.plot(model.t, model.infection_history()[:, 1], label='infected')
-    plt.legend()
-    plt.savefig('plots/other/infection_history.png')
+    plt.plot(model.t, model.infection_history()[:, 1]/model.N)
+    plt.title('Infection history (percentage of population)')
+    plt.savefig('plots/other/infected_history.png')
     plt.close()
-    plt.plot(model.t, model.infection_num_history(), label='infected_num')
+    plt.plot(model.t[infection_idx * sim_to_data_ratio], infection_data[infection_idx], label='Data')
+    plt.plot(model.t, model.infection_num_history(), label='Simulated')
+    plt.title('Daily new infections (data vs simulated)')
     plt.legend()
-    plt.savefig('plots/other/infection_num_history.png')
+    plt.savefig('plots/other/daily_infected_history.png')
     plt.close()
 
 if __name__ == '__main__':
--- optimization.py
+++ optimization.py
@@ -1,5 +1,6 @@
 from scipy.optimize import differential_evolution, OptimizeResult
 from scipy.stats import wasserstein_distance
+from scipy.special import expit
 from co_evolution_modified import opinion_dynamics
 import numpy as np
 import pandas as pd
@@ -7,13 +8,14 @@ from itertools import repeat
 from concurrent import futures
 from datetime import datetime
 
-global NUM_WORKERS; global NUM_PATH_PER_WORKER; global BINS
+global NUM_WORKERS; global NUM_PATH_PER_WORKER; global HIST_BINS
 NUM_WORKERS = 6
 NUM_PATH_PER_WORKER = 1
-NUM_AGENTS = 1000000
+NUM_AGENTS = 300000
+SIM_DATA_RATIO = 3
 HIST_BINS = np.array([-3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5])
 OPIN_BINS = (-3, -2, -1, 0, 1, 2, 3)
-INF_RATE = 0.35
+INF_RATE = 0.4
 
 def opinion_sampler(initial_opinion_data: np.ndarray, num_agents: int) -> np.ndarray:
     # Japan data is in 7 bins, we can force the opinions
@@ -28,22 +30,23 @@ def opinion_sampler(initial_opinion_data: np.ndarray, num_agents: int) -> np.nda
     # Sample uniformly between the edges to get continuous opinions
     return np.random.uniform(left_edges, right_edges)
 
-def run_simulation(parameters: tuple[float, float, int], initial_opinion: np.ndarray,
-                   initial_infected: int, max_t: int) -> tuple[np.ndarray, np.ndarray]:
+def run_simulation(parameters: tuple[float, float], initial_opinion: np.ndarray,
+                   initial_infected: int, choice: str, max_t: int) -> tuple[np.ndarray, np.ndarray]:
     """Run a single simulation with given parameters
 
     Args:
-        parameters (tuple[float, float, int]): Tuple of parameters representing 
-            the coefficient of model function for coupling opinions with infections,
-            maximum infection rate for the simulation,
-            initial number of infected for simulation
+        parameters (tuple[float, float]): Tuple of parameters for parametrizing the model function for coupling infections through external potential
+            For choice="sigmoid", we have model function(infected, opinion): parameters[0] * (1 - infected) + parameters[1] * infected * sigmoid(opinion)
+            For choice="polynomial", we have a=parameters[0] and I_crit=parameters[1] (refer to model decribed in paper by Schütte)
         initial_opinion (np.ndarray): Inital opinion for each agent sampled from PDF
+        initial_infected (int): Initial number of infected from data for simulation
+        choice (str): Choice of coupling algorithm ("polynomial", "sigmoid")
+        max_t (int): Number of time steps to simulate
 
     Returns:
         tuple[np.ndarray, np.ndarray]: Tuple of time-series of infection numbers (per timestep) and opinions over time
     """
     # TODO: Implement the actual model function for coupling opinions with infections, and its gradient
-    grad_V = np.vectorize(lambda opinion, infected: 0)
     model = opinion_dynamics(
         num_grid_points=max_t,
         max_t=max_t,
@@ -53,14 +56,16 @@ def run_simulation(parameters: tuple[float, float, int], initial_opinion: np.nda
         interaction_distance=0,
         noise_strength=0,
         stochiomatric_vectors=np.array([[-1, 1, 0], [0, -1, 1]]),
-        grad_V=grad_V,
-        inf_rate_max=parameters[0]
+        grad_V=choice,
+        grad_V_params=parameters,
+        inf_rate_max=INF_RATE
     )
     model.algo()
     return model.infection_num_history(), model.opinion_history()
 
-def composite_loss(parameters: tuple[float, float, int], opinion_data: np.ndarray, opinion_idx: np.ndarray, infection_data: np.ndarray,
-                   infection_idx: np.ndarray, initial_opinion: np.ndarray,  initial_infected: int, number_of_simulations: int) -> float:
+def composite_loss(parameters: tuple[float, float], opinion_data: np.ndarray, opinion_idx: np.ndarray, infection_data: np.ndarray,
+                   infection_idx: np.ndarray, initial_opinion: np.ndarray,  initial_infected: int, choice: str,
+                   number_of_simulations: int) -> tuple[float, float]:
     """Run multiple simulations and calculate total loss w.r.t. both opinions and infection numbers
 
     Args:
@@ -70,12 +75,12 @@ def composite_loss(parameters: tuple[float, float, int], opinion_data: np.ndarra
         infection_idx (np.ndarray): Indices where infection data is recorded
         initial_opinion (np.ndarray): Initial opinion for each agent sampled from PDF
         initial_infected (int): Initial number of infected from data for simulation
+        choice (str): Choice of coupling algorithm ("linear", "polynomial", "tanh", "absolute", "none")
         number_of_simulations (int): Number of simulation per worker
 
     Returns:
-        float: Total scaled error from all simulation for this worker
+        tuple[float, float]: Total scaled error from all simulation for this worker
     """
-    total_err = 0
     total_opinion_err = 0
     total_infection_err = 0
     # Scale of opinions error is bounded by the support of opinions, 
@@ -85,49 +90,55 @@ def composite_loss(parameters: tuple[float, float, int], opinion_data: np.ndarra
     
     for _ in range(number_of_simulations):
         # Each simulation gives new_infected and opinions at each time step
-        sim_infection, sim_opinion = run_simulation(parameters, initial_opinion, initial_infected, len(opinion_data))
+        sim_infection, sim_opinion = run_simulation(parameters, initial_opinion, initial_infected, choice, SIM_DATA_RATIO * len(opinion_data))
         
         # Discretize simulation opinions into bins and calculate wasserstein metric between actual opinions
         disc_opinions = [(np.histogram(sim_opinion[i], bins=HIST_BINS)[0])/NUM_AGENTS for i in range(len(sim_opinion))]
         opinion_err = np.mean(
-            [wasserstein_distance(OPIN_BINS, OPIN_BINS, u_weights=opinion_data[i], v_weights=disc_opinions[i]) for i in opinion_idx])
+            [wasserstein_distance(OPIN_BINS, OPIN_BINS, u_weights=opinion_data[i], v_weights=disc_opinions[i * 3]) for i in opinion_idx])
         
         # Scale the daily new infected numbers to scale of actual infectino data (per million)
         # if the agent number is too small, then Poisson jump processes resulting in only discrete
         # number of cases will blow up this error because of the ratio below
-        infection_err = np.mean(np.abs(sim_infection[infection_idx] - infection_data[infection_idx]))
+        infection_err = np.mean(np.abs(sim_infection[infection_idx * SIM_DATA_RATIO] - infection_data[infection_idx]))
         
         # Scale opinion error by bounded support of opinions, and infection error by mean of infections
         # Distinction into different errors is used for debugging
         total_opinion_err += opinion_err/opinion_err_scale
         total_infection_err += infection_err/infection_err_scale
-        total_err +=  opinion_err/opinion_err_scale + infection_err/infection_err_scale
-    # Print parameter and error of each evaluation for progress tracking
-    print(f"Parameters: {parameters}| Infection Error: {total_infection_err} | Opinion Error: {total_opinion_err}")
-    return total_err
+    
+    return total_infection_err, total_opinion_err
 
-def loss(parameters, opinion_data, opinion_idx, infection_data, infection_idx, initial_infected, pool):
+def loss(parameters, opinion_data, opinion_idx, infection_data, infection_idx, initial_infected, choice, pool):
     """ Loss function that goes into optimizer while also taking care of parallelism of simulations
 
     Returns:
-        float: Total loss from each worker
+        float: Total loss from each worker after averaging over number of simulations per worker
     """
     # Approxmating contionus opinion data using histogram weights of initial opinions, each sample from histogram is
     # mapped to intervals (-3.5, -2.5), (-2.5, -1.5), (-1.5, -0.5), (-0.5, 0.5), (0.5, 1.5), (1.5, 2.5), (2.5, 3.5)
     initial_opinion = opinion_sampler(opinion_data[0], NUM_AGENTS)
     
     # Total error collected from each worker
-    total_err = 0
-    for res in pool.map(composite_loss, repeat(parameters), repeat(opinion_data), repeat(opinion_idx), repeat(infection_data),
-                        repeat(infection_idx), repeat(initial_opinion), repeat(initial_infected), repeat(NUM_PATH_PER_WORKER, NUM_WORKERS)):
-        total_err += res
+    avg_opinion_err = 0
+    avg_infection_err = 0
+    for res in pool.map(composite_loss, repeat(parameters), repeat(opinion_data), repeat(opinion_idx),
+                        repeat(infection_data), repeat(infection_idx), repeat(initial_opinion),
+                        repeat(initial_infected), repeat(choice), repeat(NUM_PATH_PER_WORKER, NUM_WORKERS)):
+        inf_err, opin_err = res
+        avg_opinion_err += opin_err
+        avg_infection_err += inf_err
+    avg_infection_err /= NUM_WORKERS * NUM_PATH_PER_WORKER
+    avg_opinion_err /= NUM_WORKERS * NUM_PATH_PER_WORKER
     
-    return total_err
+    # Print parameter and error of each evaluation for progress tracking
+    print(f"Parameters: {parameters}| Average Infection Error: {avg_infection_err:.5f} | Average Opinion Error: {avg_opinion_err:.5f}")
+    return avg_infection_err + avg_opinion_err
 
 if __name__ == '__main__':
     # TODO: Either use the fixed maximum infection rate of 0.35 or include it as a parameter to optimize
     # TODO: Perform the optimization over the parameters of the model function for coupling opinions with infections, and its gradient
-    initial_guess = [0.35]
+    initial_guess = [0, 0]
     
     # Choose start and end dates for infection data and opinion data
     start_date = datetime(2020, 3, 12)
@@ -154,8 +165,13 @@ if __name__ == '__main__':
     infection_idx = (infection_data["new_cases"] > 0).to_numpy().nonzero()[0]
     infection_data = (infection_data['new_cases']).to_numpy()
     
+    # Choice for model function for coupling opinions with infections
+    # choices = ["polynomial", "sigmoid"]
+    # When one of the above choices is not selected, grad_V is set to 0 and opinions stay constant
+    choice = "sigmoid"
+    
     with futures.ProcessPoolExecutor(max_workers=NUM_WORKERS) as pool:
-        result = differential_evolution(func=loss, bounds=[(0.1, 1)], x0=initial_guess,
-                                        args=(opinion_data, opinion_idx, infection_data, infection_idx, initial_infected, pool),
-                                        strategy='best1bin', disp=True, popsize=3, maxiter=3, tol=0.1, workers=1)
+        result = differential_evolution(func=loss, bounds=[(-1, 1), (-1, 1)], x0=initial_guess,
+                                        args=(opinion_data, opinion_idx, infection_data, infection_idx, initial_infected, choice, pool),
+                                        strategy='best1bin', disp=True, polish=False, popsize=5, maxiter=10, tol=0.1, workers=1)
     print(result)
